2f5ada882e7c7f8aed2bd366105c24d48eefffef
ssj_100 <ssj_100@yahoo.com>
2016-07-31 14:22:47 -0400

    Init Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name justGifsApp
+ * @description
+ * # justGifsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('justGifsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name justGifsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the justGifsApp
+ */
+angular.module('justGifsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
